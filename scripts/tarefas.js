f00de335a1027fc1144da2d2b0c9ab7081a7a3fb
jhonatan <jhonatandesouzade13@gmail.com>
2022-06-29 22:46:33 -0300

    novas funcionalidades e solução dos mesmo 2

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -2,7 +2,7 @@ var form = document.querySelector(".nova-tarefa")
 var input = document.getElementById("novaTarefa")
 var pendentes = document.querySelector(".tarefas-pendentes")
 var tarefasRealizada=document.querySelector(".tarefas-terminadas")
-var nome=document.querySelector(".nome")
+var nome=document.querySelector(".nomeU")
 
 
 if (localStorage.getItem("jwt") == null || localStorage.getItem("jwt") == "") {
@@ -80,7 +80,7 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
   
   
   for(let tarefas of data){
-    if(!tarefas.completed){
+    if(tarefas.completed){
       pendentes.innerHTML += `
                 <li id="${tarefas.id}" class="tarefa">
                  <div class="not-done"></div>
@@ -88,7 +88,7 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
                 <p class="nome">ID: ${tarefas.id}</p>
                 <p class="nome">${tarefas.description}</p>
                 <p class="timestamp">${dayjs(tarefas.createdAt).format('DD/MM/YYYY') }</p>
-                <button class="btnTarefas" onclick="apagarTarefa(${tarefas.id})">apagar</button>
+                
                 </div>
                 
                 </li>
@@ -100,8 +100,9 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
           <div class="descricao">
           <p class="nome">ID: ${tarefas.id}</p>
           <p class="nome">${tarefas.description}</p>
-          <p class="timestamp">${dayjs(tarefas.createdAt).format('DD/MM/YYYY') }</p>
-          <button class="btnTarefas" onclick="apagarTarefa(${tarefas.id})">apagar</button>
+          
+          <img id="voltar" src="./img/botao-de-seta-curva-para-a-esquerda.png" alt="">
+          <img onclick="apagarTarefa(${tarefas.id})" id="lixeira" src="./img/excluir.png" alt="">
           </div>
      
      </li>
@@ -142,8 +143,10 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe', {
   }
 }).then(resposta=>
   resposta.json()).then(data=>{
+    
   // coloca o nome do cliente no ToDo
   var nomeUsuario=document.createTextNode(`${data.firstName} ${data.lastName}`)
+  nome.append(nomeUsuario)
   console.log(nomeUsuario)
   
 })
